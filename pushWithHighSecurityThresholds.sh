8c42ef1a30a7989ecb10ce6a2016c961c448d992
cvdabbeele <cvdabbeele@gmail.com>
2022-03-24 07:57:16 +0100

    cleanup

--- pushWithHighSecurityThresholds.sh
+++ pushWithHighSecurityThresholds.sh
@@ -1,27 +1,10 @@
-
+# Push of MoneyX app with high security thresholds (lots of vulnerabilities allowed)
+export CURRENTAPP="moneyx"
 STARTDIR=`pwd`
-#echo "usage: add the name of the app to be pushed as a parameter" THIS IS STILL WIP 
-#echo "       if no parameter is added, ${APP1} is pushed"   
-if [[ -z ${1} ]]; then
-    STARTDIR=`pwd`
-    cd ${APPSDIR}/moneyx
-    sed -i 's/": 0/": 300/g'  azure-pipelines.yml #change the security thresholds in the moneyx app
-    echo " "  >>README.md  #ensure that we have a change, regardless if the above sed command made anychanges
-    echo "pushing to master branch (20210303)"  
-    git add . && git commit -m "allowing risky builds"   &&  git push azure master
-    cd "${STARTDIR}"
-#else
-#    if [ "${1}" = "${APP1}" ] || [ "${1}" = "${APP2}" ] || [ "${1}" = "${APP3}" ]; then
-#            cd ${APPSDIR}/${1}
-#        sed -i 's/": 0/": 300/g'  azure-pipelines.yml #change the security thresholds in the ${1} app
-#        echo " "  >>README.md  #ensure that we have a change, regardless if the above sed command made anychanges
-#        echo "pushing to master branch "  #updated (20210303)
-#        git add . && git commit -m "allowing risky builds"   &&  git push azure master
-#        cd "${STARTDIR}"
-#    else
-#        echo "The provided parameter does not match any application"
-#        echo "try \"${APP1}\" \"${APP2}\" \"${APP3}\""
-#    fi
-fi
-    
-
+cd ${APPSDIR}/${CURRENTAPP}
+#set all thresholds in the app from 0 to 300
+sed -i 's/": 0/": 300/g'  azure-pipelines.yml #change the security thresholds in the ${CURRENTAPP} app
+echo " "  >>README.md  #ensure that we have a change, regardless if the above sed command made anychanges
+echo "pushing to internal master branch"  
+git add . && git commit -m "allowing risky builds"   &&  git push azure master
+cd "${STARTDIR}"
